f74e996e1c6b0643b48bfd73995f48428d22842c
zzbslayer <zzbslayer@sjtu.edu.cn>
2021-11-18 12:08:14 +0800

    test: add init log in test controller

--- vnf-demo/src/main/java/com/github/zzbslayer/vnfdemo/controller/TestController.java
+++ vnf-demo/src/main/java/com/github/zzbslayer/vnfdemo/controller/TestController.java
@@ -1,10 +1,18 @@
 package com.github.zzbslayer.vnfdemo.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-public class TestController {
+public class TestController implements InitializingBean {
+    @Override
+    public void afterPropertiesSet() throws Exception {
+      log.info("TestController registered.");
+    }
+
     @GetMapping("/test")
     public int test() {
         for (int i = 0; i < 1000000; i++) {}
